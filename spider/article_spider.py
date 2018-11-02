739564c12a767db5502806cf5f6b46041c168fc9
renhongl <1075220132@qq.com>
2018-11-02 12:46:41 +0800

    update

--- spider/article_spider.py
+++ spider/article_spider.py
@@ -1,16 +1,19 @@
 
 from selenium import webdriver
 from database import get_db, insert_one
-from selenium.webdriver.support import expected_conditions as EC
 import time, json, random
 
-DATABASE = 'jianshu'
 COLLECTION = 'article'
-start_page = 1
 PAGE_TOTAL = 20
+END_PAGE = 1
+
+start_page = 1
+db = get_db('jianshu')
 
 def get_browser():
-    browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get('https://www.jianshu.com')
     return browser
 
@@ -19,23 +22,43 @@ def close_browser(browser):
     browser.close()
 
 
+def scroll_down(browser):
+    SCROLL_PAUSE_TIME = 0.5
+    last_height = browser.execute_script("return document.body.scrollHeight")
+    while True:
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = browser.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
 def get_article_links(browser):
     global start_page
     global PAGE_TOTAL
+    print('Getting page %d links' % start_page)
     if start_page > 1:
-        browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        browser.find_elements_by_css_selector('.load-more').click()
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(5)
 
     arctile_links = browser.find_elements_by_css_selector('.note-list li .title')
-    print(len(arctile_links))
+    if len(arctile_links) < start_page * PAGE_TOTAL:
+        scroll_down(browser)
+        load_more = browser.find_element_by_css_selector('.load-more')
+        load_more.click()
+        time.sleep(5)
+        arctile_links = browser.find_elements_by_css_selector('.note-list li .title')
+
+    print('Total links: %d' % len(arctile_links))
+    print('Want to take: %d - %d' % ((start_page - 1) * PAGE_TOTAL, start_page * PAGE_TOTAL))
     ret = arctile_links[(start_page - 1) * PAGE_TOTAL: start_page * PAGE_TOTAL]
-    start_page = start_page + 1
     return ret
 
 
 def get_article_infor(browser, link):
-    link.click()
+    browser.execute_script("arguments[0].click();", link)
+    # link.click()
     browser.switch_to_window(browser.window_handles[1])
     arctile = {
         'title': browser.find_element_by_css_selector('.article h1').text,
@@ -52,29 +75,32 @@ def get_article_infor(browser, link):
     return arctile
 
 
-def save_to_file(file):
-    with open('./output/article/a-' + str(random.random()) + '.json', 'w', encoding='utf-8') as f:
+def save_to_file(file, file_name):
+    with open('./output/article/a-' + file_name + '.json', 'w', encoding='utf-8') as f:
         f.write(json.dumps(file, indent=4, ensure_ascii=False))
 
 
 def save_data(links, browser):
     global PAGE_TOTAL
-    global PAGE_TOTAL
+    global start_page
+    global db
+    global END_PAGE
     index = 0
-    # total = PAGE_TOTAL
-    total = 3
     for link in links:
-        infor = get_article_infor(browser, link)
-        # save_to_file(infor)
-        insert_one(db, COLLECTION, infor)
-        print('Saved: %s' % infor['title'])
         index = index + 1
-        if index > total:
-            links = get_article_links(browser)
-            save_data(links, browser)
+        infor = get_article_infor(browser, link)
+        file_name = 'Article-' + PAGE_TOTAL * (start_page - 1) + index
+        # save_to_file(infor, file_name)
+        # insert_one(db, COLLECTION, infor)
+        print('%d. Saved: %s' % ((PAGE_TOTAL * (start_page - 1) + index), infor['title']))
+            
+    start_page = start_page + 1
+    if start_page <= END_PAGE:
+        links = get_article_links(browser)
+        save_data(links, browser)
+
 
 if __name__ == '__main__':
-    db = get_db(DATABASE)
     try:
         browser = get_browser()
         links = get_article_links(browser)
